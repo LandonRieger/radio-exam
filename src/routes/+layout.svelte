335d2cfa4f97882ab5a1da80544ab25b45d04526
LandonRieger <landon.rieger@gmail.com>
2024-02-26 19:16:03 -0800

    add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
     import "../app.postcss";
+    import { dev } from '$app/environment'
+    import { inject } from '@vercel/analytics'
+
+    inject({mode: dev ? 'development' : 'production'})
 </script>
 
 <div class="bg-gray-50 dark:bg-gray-900 h-screen">
